b89d91329fbd6929ad2071b256cb7ce1708a67f0
Matthew Caruana Galizia <mattcg@gmail.com>
2013-06-21 17:30:02 -0600

    Third commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @author Matthew Caruana Galizia <m@m.cg>
+ * @copyright Copyright (c) 2013, Matthew Caruana Galizia
+ */
+
+/*jshint node:true */
+
+'use strict';
+
+var Map = require('./lib/Map');
+
+
